c318073ab7ea6fb0f36f304ad431743238a35412
Pol Marc <100007474.joan23@fje.edu>
2022-05-03 16:32:53 +0200

    first commit

--- js/hexa.js
+++ js/hexa.js
@@ -8,15 +8,13 @@ function calcular() {
     if (num2 == "") {
         resultado2.innerHTML = ""
     }
-    if (ultnum2 == 0 || ultnum2 == 1) {
+    if (ultnum2 == 0 || ultnum2 == 1 || ultnum2 == 2 || ultnum2 == 3 || ultnum2 == 4 || ultnum2 == 5 || ultnum2 == 6 || ultnum2 == 7 || ultnum2 == 8 || ultnum2 == 9 || ultnum2 == "A" || ultnum2 == "B" || ultnum2 == "C" || ultnum2 == "D" || ultnum2 == "E" || ultnum2 == "F" || ultnum2 == "a" || ultnum2 == "b" || ultnum2 == "c" || ultnum2 == "d" || ultnum2 == "e" || ultnum2 == "f") {
         decimal3.innerHTML = "Here is your decimal!"
         decimal4.innerHTML = ""
     } else {
         resultado2.innerHTML = "&#129335 Waiting for a valid binary number..."
-    }
-    if (ultnum2 >> 1 || ultnum2 >> 999999) {
-        decimal3.innerHTML = ""
-        decimal4.innerHTML = "You entered a non-hexadecimal digit (please enter a value between 0 to F)."
+        decimal4.innerHTML = "You entered a non-hexadecimal digit (please enter only 0 to 9 and A to F)"
+        decimal3.innerHTML = " "
     }
     if (ultnum2 == "") {
         decimal3 = ""
